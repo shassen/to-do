b3ae956d37fb309c7dc27a6e2cfbc343380ea4c4
Shawn Hassen <Shassen89@gmail.com>
2019-02-07 16:19:27 -0500

    Finished container components

--- to-do-client/src/App.js
+++ to-do-client/src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CreateTodo from './containers/CreateTodo';
+import Table from './containers/Table';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container" style={{ marginTop: "80px" }}>
+          <div className="row">
+            <div className="col-lg-10 offset-lg-2 col-md-10 col-sm-12 col-sx-12">
+              <CreateTodo />
+            </div>
+            <Table />
+          </div>
+        </div>
       </div>
     );
   }
